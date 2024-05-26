bb4105f77a080a612a4b525a274960288e54ece8
technicallyweb3 <mancinoweb3@gmail.com>
2024-05-26 00:17:54 -0600

    changed the API

--- src/app/api/v1/getOrders/[address]/getOrders.tsx
+++ src/app/api/v1/getOrders/[address]/getOrders.tsx
@@ -10,41 +10,86 @@ const db = mysql2.createPool({
   queueLimit: 3
 });
 
+interface TokenMetadata<T> {
+    [symbol: string]: T;
+  }
+
+let tokenDictionary: TokenMetadata<string> = {}
+
+export async function getSymbol(address: string): Promise<string> {
+
+    if (tokenDictionary[address]) return tokenDictionary[address];
+
+    const values = [address];
+
+    // Fetch buy orders
+    let sql = `
+        SELECT token_symbol
+        FROM token_metadata
+        WHERE token_hash = ?
+        LIMIT 1;
+    `;
+
+    const [symbol] = await db.query(sql, values);
+    console.log(symbol);
+    const tokenSymbol = symbol[0]?.token_symbol ? symbol[0].token_symbol : 'SPL';
+    tokenDictionary[address] = tokenSymbol
+    return tokenSymbol;
+
+}
+
 export async function getOrders(queryAddress: string) {
   try {
     const values = [queryAddress];
 
     // Fetch buy orders
     let sql = `
-      SELECT taking_mint, making_mint, taking_amount, making_amount
-      FROM jupoNjAx_txs
-      WHERE taking_mint = ?
-      ORDER BY taking_amount ASC
+        SELECT making_mint, making_amount, taking_amount, making_taking, taking_making
+        FROM orders_jupoNjAx
+        WHERE taking_mint = ? AND order_status IN ('ACTIVE', 'PARTIAL-FILL')
+        ORDER BY taking_amount ASC
     `;
     const [buyOrdersRows] = await db.query(sql, values);
 
     // Fetch sell orders
     sql = `
-      SELECT taking_mint, making_mint, taking_amount, making_amount
-      FROM jupoNjAx_txs
-      WHERE making_mint = ?
+      SELECT taking_mint, making_amount, taking_amount, making_taking, taking_making
+      FROM orders_jupoNjAx
+      WHERE making_mint = ? AND order_status IN ('ACTIVE', 'PARTIAL-FILL')
       ORDER BY making_amount DESC
     `;
+
     const [sellOrdersRows] = await db.query(sql, values);
 
     // Process and format the results
-    const buyOrders = buyOrdersRows.map(row => ({
-      taking_mint: row.taking_mint,
-      making_mint: row.making_mint,
-      taking_amount: row.taking_amount,
-      making_amount: row.making_amount
+    const buyOrders = await Promise.all(buyOrdersRows.map(async row => {
+        const takingSymbol = await getSymbol(row.taking_mint);
+        const makingSymbol = await getSymbol(row.making_mint);
+        return {
+            making_mint: row.making_mint,
+            making_amount: row.making_amount,
+            taking_amount: row.taking_amount,
+            making_taking: row.making_taking,
+            taking_making: row.taking_making,
+            BuyAmount: `${row.taking_amount} ${takingSymbol}`,
+            SellAmount: `${row.making_amount} ${makingSymbol}`,
+            Exchange: `${row.taking_making} ${takingSymbol}/${makingSymbol}`
+        };
     }));
-    
-    const sellOrders = sellOrdersRows.map(row => ({
-      taking_mint: row.taking_mint,
-      making_mint: row.making_mint,
-      taking_amount: row.taking_amount,
-      making_amount: row.making_amount
+
+    const sellOrders = await Promise.all(sellOrdersRows.map(async row => {
+        const makingSymbol = await getSymbol(row.making_mint);
+        const takingSymbol = await getSymbol(row.taking_mint);
+        return {
+            taking_mint: row.taking_mint,
+            making_amount: row.making_amount,
+            taking_amount: row.taking_amount,
+            making_taking: row.making_taking,
+            taking_making: row.taking_making,
+            BuyAmount: `${row.taking_amount} ${takingSymbol}`,
+            SellAmount: `${row.making_amount} ${makingSymbol}`,
+            Exchange: `${row.making_taking} ${makingSymbol}/${takingSymbol}`
+        };
     }));
 
     return { buyOrders, sellOrders };
--- src/app/api/v1/getOrders/[address]/route.tsx
+++ src/app/api/v1/getOrders/[address]/route.tsx
@@ -4,15 +4,23 @@ const whitelistedAddresses = [
   '25p2BoNp6qrJH5As6ek6H7Ei495oSkyZd3tGb97sqFmH'
 ]
 
+const blacklistedAddresses = [
+  'So11111111111111111111111111111111111111112',
+  'Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB',
+  'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v',
+]
+
 export async function GET(request: Request, { params }: { params: { address: string } }) {
 
-  if (whitelistedAddresses.includes(params.address)) {
-    try {
-      const response = await getOrders(params.address);
-      return new Response(JSON.stringify(response));
-    } catch (error :any) {
-      console.error('Request failed:', error);
-      return new Response(JSON.stringify({ error: error.message }), { status: 500 });
+  // if (whitelistedAddresses.includes(params.address)) {
+    if (!blacklistedAddresses.includes(params.address)) {
+      try {
+        const response = await getOrders(params.address);
+        return new Response(JSON.stringify(response));
+      } catch (error :any) {
+        console.error('Request failed:', error);
+        return new Response(JSON.stringify({ error: error.message }), { status: 500 });
+      }
     }
-  }
+  // }
 }
